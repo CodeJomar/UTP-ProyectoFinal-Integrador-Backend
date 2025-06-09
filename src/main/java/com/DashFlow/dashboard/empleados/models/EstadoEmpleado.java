2d97ab69a018d0a5dfd174ebc4de5d86e48c06c4
CodeJomar <codejomar@gmail.com>
2025-06-09 02:59:13 -0500

    creacion de evento con ENUM

--- src/main/java/com/DashFlow/dashboard/empleados/models/EstadoEmpleado.java
+++ src/main/java/com/DashFlow/dashboard/empleados/models/EstadoEmpleado.java
@@ -1,8 +1,24 @@
 package com.DashFlow.dashboard.empleados.models;
 
 public enum EstadoEmpleado {
-    ACTIVO,
-    VACACIONES,
-    AUSENTE,
-    INACTIVO
+    ACTIVO("Activo", "exito-JE"),
+    VACACIONES("Vacaciones", "advertencia-JE"),
+    AUSENTE("Ausente", "peligro-JE"),
+    INACTIVO("Inactivo", "peligro-JE");
+    
+    private final String nombre;
+    private final String cssClass;
+    
+    EstadoEmpleado(String nombre, String cssClass) {
+        this.nombre = nombre;
+        this.cssClass = cssClass;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public String getCssClass() {
+        return cssClass;
+    }
 }
